e94562e3bec8845aa4fe3938c76ade07e5f040c5
hellowangjingfei <wangxiao@wangXiaodeiMac>
2016-09-13 17:40:07 +0800

    heed

--- LBLaunchImageAdDemo/LBLaunchImageAdDemo/ViewController.m
+++ LBLaunchImageAdDemo/LBLaunchImageAdDemo/ViewController.m
@@ -18,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+- (IBAction)buttonClick:(id)sender {
 }
 
 - (void)didReceiveMemoryWarning {
