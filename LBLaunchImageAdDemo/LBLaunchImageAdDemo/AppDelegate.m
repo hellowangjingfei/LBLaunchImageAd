5a6b6593358b5aa14d67ef356200125acf48dbac
hellowangjingfei <wangxiao@wangXiaodeiMac>
2016-09-14 13:59:46 +0800

    Red

--- LBLaunchImageAdDemo/LBLaunchImageAdDemo/AppDelegate.m
+++ LBLaunchImageAdDemo/LBLaunchImageAdDemo/AppDelegate.m
@@ -43,7 +43,7 @@
     adView.clickBlock = ^(NSInteger tag){
         switch (tag) {
             case 1100:{
-                NSLog(@"点击广告回调1234");
+                NSLog(@"点击广告回调1234Red");
                 TestViewController *vc = [[TestViewController alloc]init];
                 vc.view.backgroundColor = [UIColor whiteColor];
                 [self.window.rootViewController presentViewController:vc animated:YES completion:^{
